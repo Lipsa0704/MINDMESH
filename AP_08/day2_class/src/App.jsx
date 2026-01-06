89977640ab70a2d439c375eac0eabb55c7861f48
Ananya Pradhan <ananyap777>
2026-01-06 23:47:46 +0530

    create hooks

--- AP_08/day2_class/src/App.jsx
+++ AP_08/day2_class/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Home from "./components/home";
+import "./App.css";
+
 
 const App=()=>{
   return(
--- AP_08/day2_class/src/components/Home.jsx
+++ AP_08/day2_class/src/components/Home.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home=()=>{
+  const [count,setCount]=useState(0);
+
+  useEffect(() => {
+    document.title = `Count is ${count}`;
+  }, [count]);
    return(
     <>
-      <h1>hii iam a rr</h1>
+      <div className="content">
+        <p className="para"> Number is {count}</p>
+        <button id="btn" onClick={()=>{setCount(count+1)}}>Click me</button>
+      </div>
     </>
    )
 }
