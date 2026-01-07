f1b992dd025b01a77f295e280ca0a67d227e2921
shibani2004 <shibanibardhan6@gmail.com>
2026-01-07 14:51:40 +0530

    day2

--- SB_71/ep2/src/App.jsx
+++ SB_71/ep2/src/App.jsx
@@ -1,22 +1,28 @@
 import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  // useState: declare state
   const [count, setCount] = useState(0);
 
-  // useEffect: runs after component renders
   useEffect(() => {
-    console.log("Count changed:", count);
-  }, [count]); // dependency array
+    if (count >= 5) {
+      document.body.style.backgroundColor = "#d1f7c4";
+    } else {
+      document.body.style.backgroundColor = "#f0f0f0";
+    }
+  }, [count]);
 
   return (
-    <div>
-      <h2>Count: {count}</h2>
-      <button onClick={() => setCount(count + 1)}>
-        Increase
-      </button>
+    <div className="container">
+      <h1>useState & useEffect Example</h1>
+      <p className="count">Count: {count}</p>
+
+      <div className="buttons">
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement</button>
+      </div>
     </div>
   );
 }
 
-export default App
+export default App;
