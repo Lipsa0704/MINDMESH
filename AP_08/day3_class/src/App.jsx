4664c16b0cadae24b5ffb832bb53eb73d4313ccf
Ananya Pradhan <ananyap777>
2026-01-07 21:45:24 +0530

    Creating props driling

---
+++ AP_08/day3_class/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Parent from "./component/Parent";
+const App=()=>{
+  return(
+    <>
+     <Parent  name="Ananya" place="Bhubaneswar" age="22"/>
+    
+    </>
+  )
+}
+
+export default App;
