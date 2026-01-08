94af977438d2604240563a00da04fafa8b328ed7
shibani2004 <shibanibardhan6@gmail.com>
2026-01-08 19:03:46 +0530

    day3

--- SB_71/ep3/src/App.jsx
+++ SB_71/ep3/src/App.jsx
@@ -1,43 +1,13 @@
 import React from "react";
-import "./App.css";
+import Child from "./component/Child.jsx";
 
 function App() {
-  const userName = "Shibani";
+  const message = "Hello from Parent Component";
 
   return (
-    <div className="app">
-      <h1>Props Drilling Example</h1>
-      <Parent name={userName} />
-    </div>
-  );
-}
-
-// Parent Component
-function Parent({ name }) {
-  return (
-    <div className="box parent">
-      <h2>Parent</h2>
-      <Child name={name} />
-    </div>
-  );
-}
-
-// Child Component (does not use the prop)
-function Child({ name }) {
-  return (
-    <div className="box child">
-      <h2>Child</h2>
-      <GrandChild name={name} />
-    </div>
-  );
-}
-
-// GrandChild Component (uses the prop)
-function GrandChild({ name }) {
-  return (
-    <div className="box grandchild">
-      <h2>GrandChild</h2>
-      <p>Hello, {name} 👋</p>
+    <div style={{ padding: "20px" }}>
+      <h1>Parent Component </h1>
+      <Child message={message} />
     </div>
   );
 }
