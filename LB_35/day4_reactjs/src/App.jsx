a73fd4c73a14d3036c06db2936d10f777d485f97
Lipsa0704 <bhuyanlipsalin07@gmail.com>
2026-01-09 15:19:06 +0530

    commit to day4-routing

---
+++ LB_35/day4_reactjs/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { Routes, Route } from "react-router-dom";
+import Home from './components/Home.jsx';
+import About from './components/About.jsx';
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  )
+}
+
+export default App
