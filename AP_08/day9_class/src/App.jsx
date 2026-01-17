4d9cf9e8324295c2cd75edb89d8992f7e16dd5c7
Ananya Pradhan <pradhanananya013@gmail.com>
2026-01-17 09:46:07 +0530

    Update App.jsx

--- AP_08/day9_class/src/App.jsx
+++ AP_08/day9_class/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       {isLoggedIn ? (
         <>
           <p>Welcome User!</p>
-          <button onClick={logout}>Loginn</button>
+          <button onClick={logout}>Logout</button>
         </>
       ) : (
         <button onClick={login}>Login</button>
