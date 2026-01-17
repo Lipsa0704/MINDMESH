8cd11b5870c8b96b0ed7d9f234208623800c9324
Alisha Das <alishadas831@gmail.com>
2026-01-17 12:59:12 +0530

    Server state

--- AD_101/day07-serverstate/src/main.jsx
+++ AD_101/day07-serverstate/src/main.jsx
@@ -1,4 +1,3 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -11,4 +10,3 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <App />
   </QueryClientProvider>
 );
-
