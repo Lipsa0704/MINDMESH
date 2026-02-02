5fdf1273df0f179e03b7172fe6779a5f73f0bce5
Ananya Pradhan <ananyap777>
2026-02-02 15:39:55 +0530

    performace

---
+++ AP_08/day15_class/src/App.jsx
@@ -0,0 +1,26 @@
+import { FixedSizeList } from 'react-window'
+
+const users = Array.from({ length: 1000 }, (_, i) => `User ${i + 1}`)
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Performance Optimization Demo</h2>
+
+      <FixedSizeList
+        height={300}
+        width={300}
+        itemSize={35}
+        itemCount={users.length}
+      >
+        {({ index, style }) => (
+          <div style={style}>
+            {users[index]}
+          </div>
+        )}
+      </FixedSizeList>
+    </div>
+  )
+}
+
+export default App
