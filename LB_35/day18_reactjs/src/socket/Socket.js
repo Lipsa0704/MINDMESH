f743453505507b57718c6b29983b810c78fc5fb1
Lipsa0704 <bhuyanlipsalin07@gmail.com>
2026-02-03 19:25:04 +0530

    commit to day18

---
+++ LB_35/day18_reactjs/src/socket/Socket.js
@@ -0,0 +1,8 @@
+// src/socket/socket.js
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:3000", {
+  autoConnect: false, // connect manually
+});
+
+export default socket;
